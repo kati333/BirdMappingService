cc5999cb19da93fc8216a72c06ea368cf488bfbe
Katre Karja <kati@Tianbaos-MacBook-Pro.local>
2016-10-23 01:53:29 +0300

    observed data update

--- js/main_map.js
+++ js/main_map.js
@@ -5,6 +5,20 @@ $(document).ready(function(){
 		});
             
     map.addLayer(basemap);
+    // option for the heatmap
+
+	// options for heatmap
+    var options_manualObs = {
+		minOpacity: 0.7,
+		maxzoom: 16,
+		radius: 5,
+		blur: 15,
+		 // larger scale radius 20 and blur 25, zoom 16. 
+		gradient: { .6:"#7bccc4",.7:"#4eb3d3",.8:"#2b8cbe",.9:"#0868ac",1:"#084081" }
+		// gradient: { .7:"#7bccc4",.8:"#e0f3db", .9:"#2b8cbe", 1:"#0a222e" }
+		// gradient: { .7:"#7bccc4",.9:"#e0f3db", 1:"#2b8cbe" }
+		// gradient: { .2:"#7bccc4",.3:"#80cdc1", 1:"#018571" }
+	};
 
     // add map control
 	var option_Leaflet = {
@@ -15,92 +29,34 @@ $(document).ready(function(){
 
     var observationData;
 
-	       
-	// $.ajax({
-	// 	context: this,
-	// 	type: 'GET',
-	// 	dataType: "json",
-	// 	url: 'data/testdata.json',
-	// 	timeout: 15000,
-	// 	success: function(data, textStatus, jqXHR){
-	// 		o
-	// 	},
-	// 	error: function(jqXHR, textStatus, errorThrown){
-	// 		if (textStatus==='timeout'){
-	// 			alert("timeout error");
-	// 		}
-	// 		else alert(textStatus);
-	// 	},
-	// });
+    var my_json;
 
-
-	
-	// windmill parks
-    var windmillLayer = L.geoJson();
-    var windmillStyle = {
-        // "fillColor": "#3e619b",
-        // "fillOpacity": .90,
-        // "stroke": false
-    };
-     
-    //  Add the building's overlay
-    // console.log(document.location.host);
     d3.json('data/tuulepargid.json', function(data) {
-    	// console.log(data);
-        windmillLayer.addData(data.features).setStyle(windmillStyle);
+    	my_json = L.geoJson(data.features, {
+    	    pointToLayer: function(feature, latlng) {
+    	    	 var myAngle = 45;
+				 var smallIcon = new L.Icon({
+				     iconSize: [20, 20],
+				     iconAnchor: [0, 0],
+				     iconUrl: 'images/windmill5_4.png'
+				 });
+                return L.marker(latlng, {icon: smallIcon});
+            },
+    	});
+
+    	HM_Control.addOverlay(my_json, "Windmill parks");
+	    my_json.addTo(map)
     });
-    // HM_Control.addBaseLayer(windmillLayer, "Windmill parks");
-    HM_Control.addOverlay(windmillLayer, "Windmill parks");
-    windmillLayer.addTo(map);
-
-
-	// var heat = L.heatLayer([
-	//     [58.610360, 25.657929, 0.2], 
-	//     [58.670360, 25.697929, 0.5],
-	//     [59.4369608,24.7535746, 0.7], 
-	//     [59.4328379,24.72886, 0.5],
-	//     [59.4328379,24.72886, 0.7], 
-	//     [59.4058837,24.6979822, 0.5],
-	//     [59.4328379,24.72886, 0.7], 
-	//     [59.4058837,24.6979822, 0.5],
-	//     [59.4328379,24.72886, 0.7],
-	//     [59.4058837,24.6979822, 0.5],
-	//     [59.4328379,24.72886, 0.5],
-	//     [59.4328379,24.72886, 0.7], 
-	//     [59.4058837,24.6979822, 0.5],
-	//     [59.4328379,24.72886, 0.7], 
-	//     [59.4058837,24.6979822, 0.5],
-	//     [59.4328379,24.72886, 0.7],
-	    
-
-	// ], {radius: 25}).addTo(map);
-	
-		// var startValString = data.startTime.getUTCDay() +"." + data.startTime.getUTCMonth() + "." + data.startTime.getUTCFullYear() + " " + data.startTime.getUTCHours() + ":" + data.startTime.getUTCMinutes();
-         // var endValString = start.endTime.getUTCFullYear();
-
-	
-
-	// var axis = d3.svg.axis().orient("up").ticks(4);
-	var timeSlider;
-	d3.select('#slider').call(d3.slider().axis(true).min(0).max(150).step(15)
-		.on("slide", function(evt, value){
-			//console.log(evt);
-			console.log(value);
-			$('#daterange').html("<i>" + value + "</i>");
-		}
-	));
-
-
-
 
 	$.ajax({
 		context: this,
 		type: 'GET',
 		dataType: "json",
-		url: 'data/observations.json',
+		url: 'data/data.geojson',
 		timeout: 15000,
 		success: function(data, textStatus, jqXHR){
 			showObservationHeatmap(data);
+
 
 		},
 		error: function(jqXHR, textStatus, errorThrown){
@@ -110,33 +66,75 @@ $(document).ready(function(){
 			else alert(textStatus);
 		},
 	});
+	// var axis = d3.svg.axis().orient("up").ticks(4);
+	var timeSlider, preValue;
+	preValue = 0;
+	d3.select('#slider').call(d3.slider().axis(true).min(0).max(150).step(15)
+		.on("slide", function(evt, value){
+			// console.log(evt);
+			console.log(value);
+
+			var filterData;
+
+			if(value !== preValue){
+				updateObservationHeatmap(data);
+				preValue = value;
+			}
+			filterData = filterObservations(value,data);
+			updateObservationHeatmap(filterData);
+
+			$('#daterange').html("<i>" + value + "</i>");
+		}
+	));
+
+
 
 
 
 
 	
 	// functions
-	
+	function filterObservations(value,data){
+
+	}
+
+	function updateObservationHeatmap(data){
+
+	}
+
 	function showObservationHeatmap(data) {
 		console.log(data);
-		var coords = data.adj_coords;
-		var c2 = [];
-		for(var i = 0; i<1300; i++) {
-			var t  = coords[i][0];
-			coords[i][0]=coords[i][1];
-			coords[i][1]=t;
-			coords[i].push(0.7);
-			c2.push(coords[i]);
+		var coords = data.features;
+		console.log(coords);
+
+		var coordList = [];
+
+		for(var i=0; i < coords.length; i++){
+			var t = coords[i];
+			var point = t["geometry"]["coordinates"];
+			point.push(0.8);
+			coordList.push(point);
 		}
-		// var coords = [[24.102625450014713, 59.2957067310101, 1.0], [26.34645939, 57.91454990, 1.0]];
 
-		// for (var i = 0 ; i < coords.length; i++) {
 
+
+
+		// var coords = data.adj_coords;
+		// var c2 = [];
+		// for(var i = 0; i<1300; i++) {
+		// 	var t  = coords[i][0];
+		// 	coords[i][0]=coords[i][1];
+		// 	coords[i][1]=t;
+		// 	coords[i].push(0.9);
+		// 	c2.push(coords[i]);
 		// }
 
-		var heat2 = L.heatLayer(c2, {radius: 25});
-map.addLayer(heat2);
-    HM_Control.addOverlay(heat2, "heat2");
+		var heat2 = L.heatLayer(coordList,options_manualObs);
+		map.addLayer(heat2);
+
+		// map.addData(geojsonFeature);
+
+    	HM_Control.addOverlay(heat2, "Manual bird observations");
 
 	}
 
